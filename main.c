41bcd1dda5bd6fc8ec9ecf1b6f45b0a8b433f9cf
Kross1de <awesome.hubygu@yandex.ru>
2025-05-11 10:49:18 +0600

    New logo

--- main.c
+++ main.c
@@ -5,5 +5,8 @@ void kmain(void);
 
 void kmain(void){
        Reset();
-       print("Hello from x86 os!\r\n");
+       print("\t\t\t\t\t\t===============================\r\n");
+       print("\t\t\t\t\t\t==     Hello from x86 os!    ==\r\n");
+       print("\t\t\t\t\t\t==  Copyright 2025 Kross1de  ==\r\n");
+       print("\t\t\t\t\t\t===============================\r\n");
 }
